72cf1ba05f48e4dc62a5c835c4de6e96c84762fc
wallinrex <wallinrex@gmail.com>
2025-04-30 22:54:49 -0500

    Implemented the rest of Grin

--- src/main/java/edu/grinnell/csc207/compression/Grin.java
+++ src/main/java/edu/grinnell/csc207/compression/Grin.java
@@ -1,6 +1,7 @@
 package edu.grinnell.csc207.compression;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -31,10 +32,20 @@ public class Grin {
      * BitInputStream, consuming 8 bits at a time.
      * @param file the file to read
      * @return a freqency map for the given file
+     * @throws IOException if the file cannot be opened
      */
-    public static Map<Short, Integer> createFrequencyMap (String file) {
-        // TODO: fill me in!
-        return null;
+    public static Map<Short, Integer> createFrequencyMap (String file) throws IOException{
+        BitInputStream in = new BitInputStream(file);
+        Map<Short, Integer> freqs = new HashMap<>();
+        while(in.hasBits()) {
+            short bits = (short) in.readBits(8);
+            if(freqs.containsKey(bits)) {
+                freqs.replace(bits, freqs.get(bits) + 1);
+            } else {
+                freqs.put(bits, 1);
+            }
+        }
+        return freqs;
     }
 
     /**
@@ -42,17 +53,35 @@ public class Grin {
      * .grin file denoted by outfile.
      * @param infile the file to encode.
      * @param outfile the file to write the output to.
+     * @throws IOException if either of the files cannot be opened
      */
-    public static void encode(String infile, String outfile) {
-        // TODO: fill me in!
+    public static void encode(String infile, String outfile) throws IOException {
+        BitInputStream in = new BitInputStream(infile);
+        BitOutputStream out = new BitOutputStream(outfile);
+        HuffmanTree tree = new HuffmanTree(createFrequencyMap(infile));
+        out.writeBits(1846, 32);
+        tree.serialize(out);
+        tree.encode(in, out);
     }
 
     /**
      * The entry point to the program.
      * @param args the command-line arguments.
+     * @throws IOException if either of the filenames provided can't be opened
      */
-    public static void main(String[] args) {
-        // TODO: fill me in!
-        System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
+    public static void main(String[] args) throws IOException {
+        if(args.length != 3) {
+            System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
+            return;
+        }
+        if(!args[0].equals("encode") || !args[0].equals("decode")) {
+            System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
+            return;
+        }
+        if(args[0].equals("encode")) {
+            encode(args[1], args[2]);
+        } else {
+            decode(args[1], args[2]);
+        }
     }
 }

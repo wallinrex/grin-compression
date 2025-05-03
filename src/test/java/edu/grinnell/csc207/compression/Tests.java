aabdb9fcde3bebbed907e7cc575ec8c32d3ed504
wallinrex <wallinrex@gmail.com>
2025-05-02 19:09:57 -0500

    Made sure checkstyle was good

--- src/test/java/edu/grinnell/csc207/compression/Tests.java
+++ src/test/java/edu/grinnell/csc207/compression/Tests.java
@@ -1,5 +1,10 @@
 package edu.grinnell.csc207.compression;
 
 public class Tests {
-    // TODO: fill in with your unit tests!
+    // I really didn't know what to do in terms of unit testing for this project,
+    // especially given that the Java standard PriorityQueue doesn't guarantee any
+    // ordering of elements that are "equal". Instead, I tested it both by decoding
+    // the provided .grin files and by encoding then decoding the provided .txt
+    // files. I also spent plenty of time going through individual procedures in the
+    // debugger to ensure that they worked correctly.
 }
